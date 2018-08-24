39da72a75dce67788d97218e2f968a74e3023435
ChandniSharma <chaandni.sharmaa@gmail.com>
2018-08-24 14:01:01 +0530

    Font custom

--- src/supportFiles/codeFiles/CardDeckNew.js
+++ src/supportFiles/codeFiles/CardDeckNew.js
@@ -556,7 +556,7 @@ const styles = StyleSheet.create({
     //     shadowOpacity:0.8,
     // },
     label: {
-        fontFamily:"comicsas",
+        fontFamily:"Comic Sans MS",
        //top:'2%',
         lineHeight: 200,
         width:'100%',
