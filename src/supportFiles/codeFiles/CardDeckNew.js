601da46ce7885a03f3126328ebabf0e7835234c0
deepanshu <deepanshu@bigdrop.io>
2018-08-31 11:43:18 +0530

    device width change

--- src/supportFiles/codeFiles/CardDeckNew.js
+++ src/supportFiles/codeFiles/CardDeckNew.js
@@ -314,7 +314,7 @@ componentWillUnmount() {
 
                 </View>
 
-                  <Animated.View style={{width:"100%",height:"100%",alignItems:'center',top:'-33%'}}>
+                  <Animated.View style={{width:"100%",height:"100%",alignItems:'center',top:'-31%'}}>
 
 
                       <View style={styles.card}>
